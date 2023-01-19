8bc1ae12e85a0149ab6972f488522b0f21dabe2d
Handsken <nan.222@yandex.ru>
2023-01-19 16:57:09 +0500

    homework complited

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
 import transport.Car;
 
-public class Main {
-    public Main() {
-    }
+import java.security.Key;
 
+public class Main {
     public static void main(String[] args) {
-        Car Lada = new Car("null", "", 0.0, "", 0, "null");
-        Car Audi = new Car("Audi A8", "50 L TDI quattro", 3.0, "Черного", 2020, "Германии");
-        Car BMW = new Car("BMW", "Z8", 3.0, "Черного", 2021, "Германии");
-        Car Kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "Красного", 2018, "Южной Корее");
-        Car Hyundai = new Car("Hyundai", "Avante", 1.6, "Оранжевого", 2016, "Южной Корее");
+        Car Lada = new Car("null", "", 0.0, "", 0, "null","","null","",0,false,new Car.Key(false,false));
+        Car Audi = new Car("Audi A8", "50 L TDI quattro", 3.0, "Черного", 2020, "Германии","гидромеханического","A","A256ВВ 96",4,false,new Car.Key(false,false));
+        Car BMW = new Car("BMW", "Z8", 3.0, "Черного", 2021, "Германии","автоматического","B","A256ВВ 96",2,false,new Car.Key(false,false));
+        Car Kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "Красного", 2018, "Южной Корее","гидравлического","C","A256ВВ 96",4,false,new Car.Key(false,false));
+        Car Hyundai = new Car("Hyundai", "Avante", 1.6, "Оранжевого", 2016, "Южной Корее","электромеханического","B","A256ВВ 96",4,false,new Car.Key(false,false));
+        System.out.println(Lada);
+        System.out.println(Audi);
+        System.out.println(BMW);
+        System.out.println(Kia);
+        System.out.println(Hyundai);
+        System.out.println("");
+        Car.Changetires();
         System.out.println(Lada);
         System.out.println(Audi);
         System.out.println(BMW);
         System.out.println(Kia);
         System.out.println(Hyundai);
     }
+
 }
--- src/transport/Car.java
+++ src/transport/Car.java
@@ -1,53 +1,169 @@
 package transport;
 
 public class Car {
-    private String brand;
-    private String model;
-    private double engineVolume;
-    private String color;
-    private int year;
-    private String country;
+    public static class Key {
+        private boolean keyless_entry;
+        private boolean remote_engine_start;
+        public Key (boolean keyless_entry, boolean remote_engine_start){
+            this.remote_engine_start=remote_engine_start;
+            this.keyless_entry=keyless_entry;
+            }
+        }
+        private Key key;
+        final private String brand;
+        final private String model;
+        final private double engineVolume;
+        final private String color;
+        final private int year;
+        final private String country;
+        private String transmission;
+        private String type_body;
+        private String registration_number;
+        private int seats;
+        private boolean summer_tires;
+        private static String tires;
+        public Car(String brand, String model, double engineVolume, String color, int year, String country, String transmission, String type_body, String registration_number, int seats, boolean summer_tires,Key key) {
+            if (brand == "" || brand == "null") {
+                this.brand = "default";
+            } else {
+                this.brand = brand;
+            }
+            if (model == "" || model == "null") {
+                this.model = "default";
+            } else {
+                this.model = model;
+            }
+            if (year <= 0) {
+                this.year = 2000;
+            } else {
+                this.year = year;
+            }
+            if (engineVolume <= 0) {
+                this.engineVolume = 1.5;
+            } else {
+                this.engineVolume = engineVolume;
+            }
+            if (color == "" || color == "null") {
+                this.color = "Белого";
+            } else {
+                this.color = color;
+            }
+            if (country == "" || country == "null") {
+                this.country = "default";
+            } else {
+                this.country = country;
+            }
+            if (type_body == "" || type_body == "null") {
+                this.type_body = "default body";
+            } else {
+                this.type_body = type_body;
+            }
+            if (transmission == "" || transmission == "null") {
+                this.transmission = "Механического";
+            } else {
+                this.transmission = transmission;
+            }
+            if (registration_number == "" || registration_number == "null") {
+                this.registration_number = "Отсутствует";
+            } else {
+                this.registration_number = registration_number;
+            }
+            if (seats == 0) {
+                this.seats = 4;
+            } else {
+                this.seats = seats;
+            }
+            if (summer_tires == true) {
+                tires = "Летняя";
+            } else {
+                tires = "Зимняя";
+            }
+        }
 
-    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        if (brand=="" || brand=="null"){
-            this.brand="default";
+
+
+    public static void Changetires() {
+            if (tires == "Летняя") {
+                tires = "Зимняя";
+            } else {
+                tires = "Летняя";
+            }
         }
-        else {
-            this.brand=brand;
+
+
+        public String getBrand() {
+            return brand;
         }
-        if (model=="" || model=="null"){
-            this.model="default";
+
+        public String getModel() {
+            return model;
         }
-        else {
-            this.model=model;
+
+        public int getYear() {
+            return year;
         }
-        if (year<=0){
-            this.year=2000;
+
+        public String getCountry() {
+            return country;
         }
-        else {
-            this.year = year;
+
+        public double getEngineVolume() {
+            return engineVolume;
         }
-        if (engineVolume<=0){
-            this.engineVolume=1.5;
+
+        public String getColor() {
+            return color;
         }
-        else {
-            this.engineVolume=engineVolume;
+
+        public String getTransmission() {
+            return transmission;
         }
-        if (color=="" || color=="null"){
-            this.color="Белого";
+
+        public String getType_body() {
+            return type_body;
         }
-        else {
-            this.color=color;
+
+        public String getRegistration_number() {
+            return registration_number;
         }
-        if (country=="" || country=="null"){
-            this.country="default";
+
+        public int getSeats() {
+            return seats;
         }
-        else {
-            this.country=country;
+
+        public boolean getSummer_tires() {
+            return summer_tires;
+        }
+
+        public static String getTires() {
+            return tires;
+        }
+
+        public void setTransmission(String transmission) {
+            this.transmission = transmission;
+        }
+
+        public void setType_body(String type_body) {
+            this.type_body = type_body;
+        }
+
+        public void setRegistration_number(String registration_number) {
+            this.registration_number = registration_number;
+        }
+
+        public void setSeats(int seats) {
+            this.seats = seats;
+        }
+
+        public void setSummer_tires(boolean summer_tires) {
+            this.summer_tires = summer_tires;
+        }
+
+        public static void setTires(String tires) {
+            tires = tires;
+        }
+
+        public String toString() {
+            return brand + " " + model + ", " + year + " года выпуска," + " сборка в " + country + " ," + color + " цвета," + " объем двигателя " + engineVolume + " с трансмиссией " + transmission + " типа," + " типом кузова " + type_body + ", количество мест " + seats + ", регистрационный знак " + registration_number + ", " + tires + " резина, "+"Удаленный запуск двигателя "+ key.remote_engine_start+", Бесключевой доступ "+key.keyless_entry;
         }
     }
-
-    public java.lang.String toString() {
-        return brand+" "+model+", "+year+" года выпуска,"+" сборка в "+country+" ,"+color+" цвета,"+" объем двигателя "+engineVolume;
-    }
-}
